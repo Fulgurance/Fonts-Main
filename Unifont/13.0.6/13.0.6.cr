3e7eedcc07d85b67a0c15e1fc96f526590728ac0
fulgurance <zohran.londais@gmail.com>
2023-03-12 23:31:35 +0000

    Patched Gunzip command to use the chroot (required)

--- Unifont/13.0.6/13.0.6.cr
+++ Unifont/13.0.6/13.0.6.cr
@@ -5,8 +5,9 @@ class Target < ISM::Software
 
     def prepareInstallation
         super
+
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/fonts/unifont")
-        runGunzipCommand(["-c","unifont-13.0.06.pcf.gz",">","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/fonts/unifont/unifont.pcf"],workDirectoryPath)
+        runGunzipCommand(["-c","unifont-13.0.06.pcf.gz",">","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/fonts/unifont/unifont.pcf"],workDirectoryPath)
     end
 
 end
