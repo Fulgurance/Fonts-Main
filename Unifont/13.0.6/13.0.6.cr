d1fcf6d9c117b670ee166b8326b5711602ad3b2a
Fulgurance <zohran.londais@gmail.com>
2023-05-07 23:31:48 +0100

    Patch installation error

--- Unifont/13.0.6/13.0.6.cr
+++ Unifont/13.0.6/13.0.6.cr
@@ -10,7 +10,9 @@ class Target < ISM::Software
         super
 
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/fonts/unifont")
-        runGunzipCommand(["-c","unifont-13.0.06.pcf.gz",">","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/fonts/unifont/unifont.pcf"],workDirectoryPath)
+
+        runGunzipCommand(["unifont-13.0.06.pcf.gz"],workDirectoryPath)
+        copyFile("#{workDirectoryPath(false)}/unifont-13.0.06.pcf","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/fonts/unifont/unifont.pcf")
     end
 
 end
