37b23966f8fb9312c3c4a461fd148702095bcf62
Fulgurance <zohran.londais@gmail.com>
2024-11-27 18:25:59 +0000

    Updated installer

--- Hack-Fonts/3.3.0/3.3.0.cr
+++ Hack-Fonts/3.3.0/3.3.0.cr
@@ -3,6 +3,8 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}")
+
         moveFile(   path:       "#{buildDirectoryPath}/usr",
                     newPath: "#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr")
     end
